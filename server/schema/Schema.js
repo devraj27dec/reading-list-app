6e811bbf9937c35677e7025ce4426684db9766cc
devraj27dec <rajkirar2002.pc@gmail.com>
2024-03-16 14:32:32 +0530

    Mutations

--- server/schema/Schema.js
+++ server/schema/Schema.js
@@ -1,31 +1,35 @@
 const graphql = require('graphql')
+const { Author } = require('../models/author.model.js')
+const { Book } = require('../models/book.model.js')
+
 
 const { GraphQLSchema,
     GraphQLObjectType , 
     GraphQLID ,
     GraphQLString,
     GraphQLInt ,
-    GraphQLList } = graphql;
+    GraphQLList,
+    GraphQLNonNull } = graphql;
 
 
 const _ = require('lodash');
 
 
-// var books = [
-//     {name: 'Name of the Wind', genre: 'Fantasy', id: '1' , authorId: "1"},
-//     {name: 'The final Emperor', genre: 'Fantasy', id: '2' , authorId: "2"},
-//     {name: 'The Long Earth', genre: 'Sci-Fi', id: '3' , authorId: "3"},
-//     {name: 'The Hero Age', genre: 'Fantasy', id: '4' , authorId: "1"},
-//     {name: 'The Color of magic', genre: 'Fantasy', id: '5' , authorId: "2"},
-//     {name: 'The Light Fatntastic', genre: 'Fantasy', id: '6' , authorId: "2"},
-// ]
+var books = [
+    {name: 'Name of the Wind', genre: 'Fantasy', id: '1' , authorId: "1"},
+    {name: 'The final Emperor', genre: 'Fantasy', id: '2' , authorId: "2"},
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3' , authorId: "3"},
+    {name: 'The Hero Age', genre: 'Fantasy', id: '4' , authorId: "1"},
+    {name: 'The Color of magic', genre: 'Fantasy', id: '5' , authorId: "2"},
+    {name: 'The Light Fatntastic', genre: 'Fantasy', id: '6' , authorId: "2"},
+]
 
 
-// var authors = [
-//     {name: "Ashneer Grover" , age: 50, id: '1' },
-//     {name: "Tanya partap" , age: 45, id: '2'},
-//     {name: "Hitesh Choudhary" , age: 40, id: '3' }
-// ]
+var authors = [
+    {name: "Ashneer Grover" , age: 50, id: '1' },
+    {name: "Tanya partap" , age: 45, id: '2'},
+    {name: "Hitesh Choudhary" , age: 40, id: '3' }
+]
 
 
 /**Book Schema **/
@@ -63,6 +67,7 @@ const AuthorType = new GraphQLObjectType({
 })
 
 
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -96,6 +101,41 @@ const RootQuery = new GraphQLObjectType({
     }   
 })
 
+
+
+// mutation allows to mutate or change our data having data deleting data &  updating data
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args:{
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
+            },
+            async resolve(parent ,args) {
+                const author = new Author({ ...args });
+                return await author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args:{
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                genre: {type: new GraphQLNonNull(GraphQLString)},
+                authorId: {type: new GraphQLNonNull(GraphQLString)},
+            },
+            async resolve(parent ,args) {
+                const book = new Book({ ...args });
+                return await book.save();
+            }
+        },
+    }
+})
+
+
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
